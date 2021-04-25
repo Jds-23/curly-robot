6f83aaeb838dfea98d8167b9c99a12715563e68d
Joydeep Singha <67229156+Jds-23@users.noreply.github.com>
2021-04-25 17:00:16 +0530

    Initial commit

---
+++ component/header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-logo">
+                Brand
+            </div>
+            <div className="header-nav">
+                <div className="header-nav-item">Home</div>
+                <div className="header-nav-item">Portfolio</div>
+                <div className="header-nav-item">Blog</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
